c60c021b8b385705ebf9df3dc1a5df35080073c4
JosipKuci <josipsimunkuci@gmail.com>
2024-10-30 12:24:57 +0100

    add flag and register function prototypes

--- include/gb_cpu.h
+++ include/gb_cpu.h
@@ -1,7 +1,7 @@
 #ifndef GB_CPU_H
 #define GB_CPU_H
-#endif
 #include <stdbool.h>
+#include <stdlib.h>
 #include "gb_registers.h"
 #include "gb_instructions.h"
 #include "gb_emulator.h"
@@ -15,10 +15,26 @@ struct gb_cpu_info
     
     bool is_halted;
     bool is_stepping;
+    bool is_master_interrupt_enabled;
 
     struct gb_instruction *current_instruction;
 };
+//Checks if the 7th bit of the flags register (the Zero flag) is set
+int get_cpu_z_flag(struct gb_cpu_info* cpu_info);
+//Checks if the 4th bit of the flags register (the Carry flag) is set
+int get_cpu_c_flag(struct gb_cpu_info* cpu_info);
+
+void gb_cpu_set_flags(struct gb_cpu_info* cpu_info, uint8_t z, uint8_t n, uint8_t h, uint8_t c);
+void gb_cpu_set_register(enum register_type, uint16_t value);
+
+struct gb_registers* gb_get_all_registers();
 
 void gb_cpu_init();
 bool gb_cpu_step();
 uint16_t gb_cpu_read_register(enum register_type register);
+
+typedef void (*IN_PROC)(struct gb_cpu_info*);
+
+IN_PROC instruction_get_processor(enum instruction_type type);
+
+#endif
