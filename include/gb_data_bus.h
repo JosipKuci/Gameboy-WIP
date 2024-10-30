d8360d59d76a1d8e0d67f72d0834e70833930847
JosipKuci <josipsimunkuci@gmail.com>
2024-10-30 12:25:20 +0100

    fix data bus header

--- include/gb_data_bus.h
+++ include/gb_data_bus.h
@@ -1,6 +1,9 @@
 #ifndef GB_DATA_BUS_H
 #define GB_DATA_BUS_H
-#endif
+ 
 #include<stdint.h>
 uint8_t gb_bus_read(uint16_t address);
+uint16_t gb_bus_read_16(uint16_t address);
 void gb_bus_write(uint8_t value, uint16_t address);
+void gb_bus_write_16(uint16_t value, uint16_t address);
+#endif
