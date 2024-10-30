cea4f1fe00f10531723d6c6c602cda8d57442920
JosipKuci <josipsimunkuci@gmail.com>
2024-10-30 12:25:40 +0100

    fix emulator header

--- include/gb_emulator.h
+++ include/gb_emulator.h
@@ -1,6 +1,5 @@
 #ifndef GB_EMULATOR_H
 #define GB_EMULATOR_H
-#endif
 #include<stdbool.h>
 #include<stdint.h>
 struct gb_emulator_info
@@ -14,3 +13,4 @@ int gb_emulator_init(int argc, char **argv);
 struct gb_emulator_info* gb_emulator_get_info();
 void gb_emulator_cycle(int cpu_cycles);
 void gb_emulator_set_info(struct gb_emulator_info* emulator_info);
+#endif
