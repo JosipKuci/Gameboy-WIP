618b62e2643ff74f640e7e13396d4cf88b42cd9c
JosipKuci <josipsimunkuci@gmail.com>
2025-02-17 21:18:22 +0100

    add dma and ppu ticks, ppu initialization

--- src/gb_emulator.c
+++ src/gb_emulator.c
@@ -1,5 +1,6 @@
 #include "gb_emulator.h"
 #include "gb_cpu.h"
+#include "gb_dma.h"
 #include "gb_interface.h"
 #include "gb_timer.h"
 #include "gb_cartridge.h"
@@ -7,6 +8,7 @@
 #include "SDL2/SDL_ttf.h"
 #include <stdio.h>
 #include <pthread.h>
+#include "gb_ppu.h"
 static struct gb_emulator_info emulator_info;
 struct gb_emulator_info* gb_emulator_get_info()
 {
@@ -20,11 +22,15 @@ void gb_emulator_set_info(struct gb_emulator_info* emulator_info)
 }
 void gb_emulator_cycle(int cpu_cycles)
 {
-    int number_of_cycles=cpu_cycles*4;
-    for(int i=1; i<=number_of_cycles;i++)
+    for(int i=1; i<=cpu_cycles;i++)
     {
-        emulator_info.timer_ticks++;
-        gb_timer_tick();
+        for(int j=1;j<=4;j++)
+        {
+            emulator_info.timer_ticks++;
+            gb_timer_tick();
+            gb_ppu_tick();
+        }
+        gb_dma_tick();
     }
     
 }
@@ -36,6 +42,7 @@ void *gb_cpu_run(void *p)
 {
     gb_cpu_init();
     gb_timer_init();
+    gb_ppu_initialize();
     emulator_info.is_running=true;
     emulator_info.is_paused=false;
     emulator_info.timer_ticks=0;
@@ -57,6 +64,7 @@ int gb_emulator_init(int argc, char **argv)
 {
     gb_initialize_interface();
     
+
     gb_emulator_set_info(&emulator_info);
     if (!gb_load_cartridge(argv[1])) {
         printf("Failed to load ROM file: %s\n", argv[1]);
@@ -68,10 +76,16 @@ int gb_emulator_init(int argc, char **argv)
         fprintf(stderr, "FAILED TO START MAIN CPU THREAD!\n");
         return -1;
     }
+    uint32_t previous_frame=0;
     while(!emulator_info.is_killed)
     {
         _sleep(1);
         gb_interface_handle_events();
+        if(previous_frame != gb_ppu_get_info()->current_frame)
+        {
+            gb_ui_update_debug_window();
+            previous_frame=gb_ppu_get_info()->current_frame;
+        }
     }
     return 0;
 }
