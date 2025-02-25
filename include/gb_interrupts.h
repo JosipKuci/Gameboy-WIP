aaba5203c40853a85a1a8b51f6dddb5c5bfa8122
JosipKuci <josipsimunkuci@gmail.com>
2025-02-25 10:05:38 +0100

    remove function prototype for interrupt request

--- include/gb_interrupts.h
+++ include/gb_interrupts.h
@@ -7,8 +7,7 @@ enum gb_interrupt_type{
     IT_LCD_STAT = 2,
     IT_TIMER = 4,
     IT_SERIAL = 8,
-    IT_JOYPAD = 16
+    IT_JOYPAD = 16,
 };
-void gb_cpu_handle_interrupts(struct gb_cpu_info* cpu_info);
 void gb_cpu_request_interrupt(enum gb_interrupt_type interrupt);
 #endif
