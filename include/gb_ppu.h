382860afb9cc6868604c698e6642192d93f4e539
JosipKuci <josipsimunkuci@gmail.com>
2025-02-25 10:06:31 +0100

    add sprite loading and expand ppu struct file

--- include/gb_ppu.h
+++ include/gb_ppu.h
@@ -16,14 +16,69 @@ struct gb_oam_data{ //"Object attribute memory"
     uint8_t attributes; //Priority-Y_flip-X_flip-DMG_Palette-Bank
 };
 
+struct gb_oam_in_line{
+    struct gb_oam_data object;
+    struct gb_oam_in_line* next_object;
+};
+
+
+enum FIFO_pixel_fetch{
+    GET_TILE,
+    GET_TILE_DATA_LOW,
+    GET_TILE_DATA_HIGH,
+    SLEEP,
+    PUSH,
+};
+
+struct gb_FIFO_element{
+    struct gb_FIFO_element* next_element;
+    uint32_t color_value;
+};
+
+struct gb_FIFO_queue{
+    struct gb_FIFO_element* first_element;
+    struct gb_FIFO_element* last_element;
+    uint32_t lenght;
+};
+
+struct gb_FIFO_queue_info{
+    enum FIFO_pixel_fetch fetch_state;
+    struct gb_FIFO_queue FIFO_queue;
+    uint8_t line_x_pos;
+    uint8_t pushed_x;
+    uint8_t fetched_x;
+    uint8_t bg_data[3];
+    uint8_t oam_data[6];
+    uint8_t map_y;
+    uint8_t map_x;
+    uint8_t tile_y;
+    uint8_t fifo_x;
+};
+
+
+
+
 struct gb_ppu_info{
     struct gb_oam_data oam_ram[40];
     uint8_t vram[8192];
+    struct gb_FIFO_queue_info FIFO_info;
 
     uint32_t current_frame;
     uint32_t line_ticks;
     uint32_t* video_buffer;
+    uint8_t window_line;
+
+    struct gb_oam_in_line* sprites_in_line;
+    uint8_t num_of_sprites; // There is at most 10 sprites per line (DMG-acid2)
+    struct gb_oam_in_line list_of_sprite_line_entries[10]; // Used to store memory spaces of specific sprites
+
+    //For FIFO sprite fetching
+    uint8_t fetched_entry_count; //Max of 3 per pixel fetch
+    struct gb_oam_data fetched_entries[3];
 };
+
+
+
 
 struct gb_ppu_info* gb_ppu_get_info();
 
@@ -35,4 +90,6 @@ uint8_t gb_ppu_vram_read(uint16_t address);
 
 void gb_ppu_initialize();
 void gb_ppu_tick();
+
+uint8_t gb_ppu_is_window_visible();
 #endif
--- src/gb_ppu.c
+++ src/gb_ppu.c
@@ -1,6 +1,7 @@
 #include "gb_ppu.h"
 #include <string.h>
 #include "gb_lcd.h"
+#include "gb_ppu_pipeline.h"
 #include "gb_cpu.h"
 #include "SDL2/SDL.h"
 static uint32_t target_frame_time=1000/60;
@@ -8,7 +9,7 @@ static long previous_frame_time=0;
 static long start_timer = 0;
 static long frame_count = 0;
 
-static struct gb_ppu_info ppu_info;
+struct gb_ppu_info ppu_info;
 struct gb_ppu_info* gb_ppu_get_info()
 {
     return &ppu_info;
@@ -18,7 +19,21 @@ void gb_ppu_initialize()
 {
     ppu_info.line_ticks=0;
     ppu_info.current_frame=0;
-    ppu_info.video_buffer=malloc(Y_RESOLUTION*X_RESOLUTION*sizeof(uint32_t));
+    ppu_info.video_buffer=malloc(Y_RESOLUTION*X_RESOLUTION*sizeof(32));
+    ppu_info.window_line=0;
+
+    //FIFO
+    ppu_info.FIFO_info.line_x_pos=0;
+    ppu_info.FIFO_info.pushed_x=0;
+    ppu_info.FIFO_info.fetched_x=0;
+    ppu_info.FIFO_info.FIFO_queue.lenght=0;
+    ppu_info.FIFO_info.FIFO_queue.first_element=NULL;
+    ppu_info.FIFO_info.FIFO_queue.last_element=NULL;
+    ppu_info.FIFO_info.fetch_state=GET_TILE;
+
+
+    ppu_info.sprites_in_line=0;
+    ppu_info.fetched_entry_count=0;
     gb_lcd_initialize();
     gb_lcd_set_ppu_mode(OAMSCAN);
     memset(ppu_info.oam_ram,0,sizeof(ppu_info.oam_ram));
@@ -53,12 +68,19 @@ uint8_t gb_ppu_vram_read(uint16_t address)
     address-=0x8000;
     return ppu_info.vram[address];
 }
-
+uint8_t gb_ppu_is_window_visible()
+{
+    return gb_lcd_is_window_enabled() && gb_get_lcd_info()->window_x>=0 && gb_get_lcd_info()->window_x <= 166 && gb_get_lcd_info()->window_y >= 0 && gb_get_lcd_info()->window_y < Y_RESOLUTION;
+}
 
 void gb_ppu_hblank()
 {
     if(ppu_info.line_ticks>=DOTS_PER_LINE)
     {
+        if(gb_ppu_is_window_visible() && gb_get_lcd_info()->lcd_y >= gb_get_lcd_info()->window_y && gb_get_lcd_info()->lcd_y < gb_get_lcd_info()->window_y+Y_RESOLUTION)
+        {
+            ppu_info.window_line++;
+        }
         gb_get_lcd_info()->lcd_y++;
         if(gb_get_lcd_info()->lcd_y==gb_get_lcd_info()->ly_compare)
         {
@@ -75,7 +97,7 @@ void gb_ppu_hblank()
         {
             gb_lcd_set_ppu_mode(VBLANK);
             gb_cpu_request_interrupt(IT_VBLANK);
-            if(gb_lcd_is_interrupt_selected(VBLANK))
+            if(gb_lcd_is_interrupt_selected(MODE1_SELECT))
             {
                 gb_cpu_request_interrupt(IT_LCD_STAT);
             }
@@ -111,6 +133,10 @@ void gb_ppu_vblank()
 {
     if(ppu_info.line_ticks>=DOTS_PER_LINE)
     {
+        if(gb_ppu_is_window_visible() && gb_get_lcd_info()->lcd_y >= gb_get_lcd_info()->window_y && gb_get_lcd_info()->lcd_y < gb_get_lcd_info()->window_y+Y_RESOLUTION)
+        {
+            ppu_info.window_line++;
+        }
         gb_get_lcd_info()->lcd_y++;
         if(gb_get_lcd_info()->lcd_y==gb_get_lcd_info()->ly_compare)
         {
@@ -127,8 +153,64 @@ void gb_ppu_vblank()
         {
             gb_lcd_set_ppu_mode(OAMSCAN);
             gb_get_lcd_info()->lcd_y=0;
+            ppu_info.window_line=0;
         }
         ppu_info.line_ticks=0;
+
+    }
+}
+
+void gb_ppu_load_sprites_in_line()
+{
+    int current_y_line=gb_get_lcd_info()->lcd_y;
+    uint8_t sprite_height=gb_lcd_get_obj_size();
+    memset(ppu_info.list_of_sprite_line_entries,0,sizeof(ppu_info.list_of_sprite_line_entries));
+    //Going through all OAM entries
+    for(int i=0;i<40;i++)
+    {
+        struct gb_oam_data oam_data=ppu_info.oam_ram[i];
+        if(oam_data.x==0)//Means data is not visible
+        {
+            continue;
+        }
+        if(ppu_info.num_of_sprites>=10)
+        {
+            break;
+        }
+        if(oam_data.y <= current_y_line+16 && oam_data.y + sprite_height > current_y_line+16)
+        {
+            //Sprite is on current line
+            struct gb_oam_in_line* current_line_object = &ppu_info.list_of_sprite_line_entries[ppu_info.num_of_sprites++]; 
+            current_line_object->object=oam_data;
+            current_line_object->next_object=NULL;
+            if(!ppu_info.sprites_in_line || ppu_info.sprites_in_line->object.x > oam_data.x)
+            {
+                current_line_object->next_object=ppu_info.sprites_in_line;
+                ppu_info.sprites_in_line=current_line_object;
+                continue;
+            }
+            //Sorting of oam line objects
+            struct gb_oam_in_line *temp = ppu_info.sprites_in_line;
+            struct gb_oam_in_line *previous=temp;
+            while(temp)
+            {
+                if(temp->object.x>oam_data.x)
+                {
+                    previous->next_object=current_line_object;
+                    current_line_object->next_object=temp;
+                    break;
+                }
+
+                if(!temp->next_object)
+                {
+                    temp->next_object=current_line_object;
+                    break;
+                }
+
+                previous=temp;
+                temp=temp->next_object;
+            }
+        }
 
     }
 }
@@ -138,14 +220,33 @@ void gb_ppu_oam_scan()
     if(ppu_info.line_ticks>=80)
     {
         gb_lcd_set_ppu_mode(DRAWING);
+        ppu_info.FIFO_info.fetch_state=GET_TILE;
+        ppu_info.FIFO_info.line_x_pos=0;
+        ppu_info.FIFO_info.fetched_x=0;
+        ppu_info.FIFO_info.pushed_x=0;
+        ppu_info.FIFO_info.fifo_x=0;
+    }
+
+    if(ppu_info.line_ticks==1)
+    {
+        ppu_info.sprites_in_line=0;
+        ppu_info.num_of_sprites=0;
+
+        gb_ppu_load_sprites_in_line();
     }
 }
 
 void gb_ppu_drawing()
 {
-    if(ppu_info.line_ticks>=80+172)
+    gb_ppu_pipeline_process();
+    if(ppu_info.FIFO_info.pushed_x>=X_RESOLUTION)
     {
+        gb_ppu_pipeline_FIFO_reset();
         gb_lcd_set_ppu_mode(HBLANK);
+        if(gb_lcd_is_interrupt_selected(MODE0_SELECT))
+        {
+            gb_cpu_request_interrupt(IT_LCD_STAT);
+        }
     }
 }
 
